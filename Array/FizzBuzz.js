dff767f17b6e128ea7cab65e90d68533e9e23223
Vishwanath20 <visupater@gmail.com>
2022-09-28 17:21:28 +0530

    1 file modified

--- Array/FizzBuzz.js
+++ Array/FizzBuzz.js
@@ -1,18 +1,17 @@
-
+// New Code
 function fizzBuzz(n1, n2) {
-	for(let i=n1; i<=n2; i++) {
-        
-        let find3 = i.toString().indexOf('3') > -1;
-        let find5 = i.toString().indexOf('5') > -1;
-        if(find3) {
-            console.log('Fizz');
-            continue;
-        } else if(find5){
-            console.log('Buzz');
-        } else {
-            console.log(i);
-        }
+  for (let i = n1; i <= n2; i++) {
+    let find3 = i.toString().indexOf('3') > -1;
+    let find5 = i.toString().indexOf('5') > -1;
+    if (find3) {
+      console.log('Fizz');
+      continue;
+    } else if (find5) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
     }
+  }
 }
 let N1 = 1;
 let N2 = 10;
