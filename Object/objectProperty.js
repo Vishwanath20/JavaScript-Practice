3275f0b591476953971d4db1e3440f487a3bd86e
Vishwanath20 <visupater@gmail.com>
2024-04-06 19:36:56 +0530

    added new program

--- Object/objectProperty.js
+++ Object/objectProperty.js
@@ -5,3 +5,27 @@ var student = {
 };
 
 console.log(student)
+
+console.log(student.name);
+
+console.log(student["name"]);
+
+// Creating js objects with object literal
+let car = {
+	name : 'GT',
+	maker : 'BMW',
+	engine : '1998cc'
+};
+// Property accessor
+console.log(car.name); // GT dot notation
+console.log(car['maker']); //BMW bracket notation
+//console.log(car[maker]);  //-------ReferenceError: maker is not defined
+
+
+
+
+let x = '{ "b": 1, "c": 2 }'; 
+let y = JSON.parse(x); 
+console.log( y); // { b: 1, c: 2 }
+console.log(typeof y); //object
+
